c3750aab1b37156a228896b687a252d23f0aea6a
Ossian O'Reilly <ooreilly@stanford.edu>
2018-01-02 22:55:27 -0800

    Add template option to command.

--- gdocstr/command.py
+++ gdocstr/command.py
@@ -10,6 +10,7 @@ class Command(object):
     """
 
     def __init__(self, options):
+        import os
         from . import extract
         from . import parse
         self.filename = options['<file>']
@@ -18,11 +19,20 @@ class Command(object):
         else:
             self.name = options['<name>']
         self.docstring = extract.extract(self.filename, self.name)
-        self.parser = parse.parser(self.docstring, 'Google')
+        self.parser = parse.parser(self.docstring['docstring'], 'Google')
         self.parser.parse()
         self.options = {'--text' : self.text,
                         '--markdown' : self.markdown,
                         '--json' : self.json}
+
+        if options['--template']:
+            self.template = options['--template'][1:]
+        else:
+            self.template = ''
+
+        if not self.template:
+            self.template = os.path.join(os.path.dirname(__file__),
+                                         'templates/google_docstring.md')
 
     def __call__(self, cmd):
         """
@@ -37,12 +47,31 @@ class Command(object):
 
     def text(self):
         """
-        Output docstring as plain-text."
+        Output docstring as plain-text.
         """
-        print(self.parser)
+        txt = ''
+        if self.docstring['class']:
+            txt += self.docstring['class']
+            if self.docstring['function']:
+                txt += '.'
+        for prop in ['function', 'signature']:
+            if prop in self.docstring:
+                txt += self.docstring[prop]
+        txt += self.docstring['docstring']
+        print(txt)
 
-    def markdown(self, template=None):
-        print(self.parser.__markdown__(template))
+    def markdown(self):
+        """
+        Output docstring as markdown using a template.
+        """
+        from mako.template import Template
+        template = Template(filename=self.template)
+        hd1 = '#'
+        hd2 = '##'
+        hd3 = '###'
+        headers, data = self.parser.markdown()
+        print(template.render(header=self.docstring, sections=data,
+                              headers=headers, h1=hd1, h2=hd2, h3=hd3))
 
     def json(self):
         """
