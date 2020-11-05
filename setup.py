176414206e2239801c2f19f8a19d5bb837c29d84
Ossian O'Reilly <ossian.oreilly@gmail.com>
2020-11-05 10:56:23 -0800

    Bump version: 0.2.5 -> 0.2.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
  
 setup(name='mydocstring',
-      version='0.2.5',
+      version='0.2.6',
       description="""A tool for extracting and converting Google-style docstrings to
       plain-text, markdown, and JSON.""",
       url='http://github.com/ooreilly/mydocstring',
